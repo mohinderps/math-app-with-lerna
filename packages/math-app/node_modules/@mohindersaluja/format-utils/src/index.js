3f32cf32a79500c589e9d8ea7ef76a9a6056b43d
mohinderps <mohin19derp@gmail.com>
2020-03-10 22:59:05 +0530

    example of package that depends on my other npm packages

---
+++ packages/math-app/node_modules/@mohindersaluja/format-utils/src/index.js
@@ -0,0 +1,3 @@
+const chalk = require('chalk');
+
+module.exports = str => chalk.blue(str);
---
+++ packages/math-app/src/index.js
@@ -0,0 +1,4 @@
+const math = require('@mohindersaluja/basic-math-utils');
+const format = require('@mohindersaluja/format-utils');
+
+console.log(format(math.add(1,2)));
