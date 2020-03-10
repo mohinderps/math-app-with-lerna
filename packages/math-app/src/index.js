3600773aff81e47a6315388e4164e8d4cade66b8
mohinderps <mohin19derp@gmail.com>
2020-03-11 01:40:08 +0530

    lerna commit

--- packages/math-app/src/index.js
+++
@@ -1,4 +0,0 @@
-const math = require('@mohindersaluja/basic-math-utils');
-const format = require('@mohindersaluja/format-utils');
-
-console.log(format(math.add(1,2)));
---
+++ packages/package-c/src/index.js
@@ -0,0 +1,4 @@
+const math = require('@mohindersaluja/math-app-p1');
+const format = require('@mohindersaluja/math-app-p2');
+
+console.log(format(math.add(1,2)));
